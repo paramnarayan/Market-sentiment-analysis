1004028035241770d641d9a20af6e40affa35855
Param Narayan Mehrotra <paramnarayanmehrotra.pnm@gmail.com>
2026-02-07 00:00:01 +0530

    cache time update

--- streamlitpage.py
+++ streamlitpage.py
@@ -5,7 +5,7 @@ from main import news_sentiment,gen_report
 st.set_page_config(page_title="Market Sentiment Analysis")
 st.title("Market Sentiment Analysis Dashboard")
 
-@st.cache_data(ttl=3600)
+@st.cache_data(ttl=36)
 def get_cached_report():
     return gen_report()
 if st.button("Generate Market Sentiment Report"):
